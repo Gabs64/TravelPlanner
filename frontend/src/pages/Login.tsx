ce6c461b131cfdce7c4095aad0deff5dcdfeacf5
G4bz ‎ <m440845@gmail.com>
2026-09-14 02:18:55 +0800

    style: remove unused imports in Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FaCheckCircle, FaPlane, FaBell, FaEnvelope } from "react-icons/fa";
+import { FaCheckCircle, FaPlane } from "react-icons/fa";
 import API_BASE from "../apiConfig";
 import "./Login.css";
 
